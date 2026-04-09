4956507591423caf965161edde8a22aa8411d9e3
muskan-codes-22 <musukumari2211@gmail.com>
2026-04-09 15:04:20 +0530

    feat: add welcome message and create About component

--- reactapp/vite-project/src/App.jsx
+++ reactapp/vite-project/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const name="Muskan"
 
   return (
     <>
@@ -17,6 +18,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h2>Welcome, {name}!</h2>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
